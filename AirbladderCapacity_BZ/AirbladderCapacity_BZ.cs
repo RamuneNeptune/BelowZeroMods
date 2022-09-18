6e87dd646237a1ec7ddfab7581818938a04b736d
ramennoodlesxv <94365980+ramennoodlesxv@users.noreply.github.com>
2022-09-18 23:58:15 +1200

    Add files via upload

---
+++ AirbladderCapacity_BZ/AirbladderCapacity_BZ.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using Logger = QModManager.Utility.Logger;
+
+namespace AirbladderCapacity_BZ
+{
+    class AirbladderCapacity
+    {
+        [HarmonyPatch(typeof(PlayerTool))]
+        [HarmonyPatch("Awake")]
+        internal class PatchPlayerToolAwake
+        {
+            [HarmonyPostfix]
+            public static void Postfix(PlayerTool __instance)
+            {
+                if (__instance.GetType() == typeof(AirBladder))
+                {
+                    AirBladder airbladder = __instance as AirBladder;
+
+                    float oxygenModifier = QMod.Config.oxygenCap;
+                    airbladder.oxygenCapacity = oxygenModifier;
+
+                    Logger.Log(Logger.Level.Info, $"Modified oxygen = {oxygenModifier}", null, true);
+                }
+            }
+        }
+    }
+}
