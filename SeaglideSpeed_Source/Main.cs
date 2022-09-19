db126de2ac10460aa217310498f59405c1c41109
ramennoodlesxv <94365980+ramennoodlesxv@users.noreply.github.com>
2022-09-20 03:00:17 +1200

    Add files via upload

---
+++ SeaglideSpeed_Source/Main.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using QModManager.API.ModLoading;
+using Logger = QModManager.Utility.Logger;
+using UnityEngine;
+
+using SMLHelper.V2.Json;
+using SMLHelper.V2.Options.Attributes;
+using SMLHelper.V2.Handlers;
+
+
+namespace SeaglideSpeed_BZ
+{
+    [QModCore]
+    public static class QMod
+    {
+        internal static Config Config { get; private set; }
+
+        [QModPatch]
+        public static void Patch()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var modName = ($"RamuneNeptune_{assembly.GetName().Name}");
+            Harmony harmony = new Harmony(modName);
+            harmony.PatchAll(assembly);
+
+            Config = OptionsPanelHandler.Main.RegisterModOptions<Config>();
+            Logger.Log(Logger.Level.Info, "Patched successfully!", null, true);
+        }
+    }
+
+    [Menu("Airbladder Capacity BZ")]
+    public class Config : ConfigFile
+    {
+        [Slider("Seaglide Speed", Format = "{0:F2}", Min = 25.0F, Max = 100.0F, DefaultValue = 25.0F, Step = 1.0F, Tooltip = "Re-select Seaglide to update")]
+        public float speedVal = 25.0F;
+        [Slider("Seaglide Acceleration", Format = "{0:F2}", Min = 36.56F, Max = 75.0F, DefaultValue = 36.56F, Step = 1.0F, Tooltip = "Re-select Seaglide to update")]
+        public float accelVal = 36.56F;
+    }
+}
---
+++ SeaglideSpeed_Source/SeaglideSpeed.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using Logger = QModManager.Utility.Logger;
+using UnityEngine;
+
+namespace SeaglideSpeed_BZ
+{
+    class SeaglideSpeed
+    {
+        [HarmonyPatch(typeof(PlayerController))]
+        [HarmonyPatch("Start")]
+        internal class PatchPlayerControllerStart
+        {
+            [HarmonyPostfix]
+            public static void Postfix(PlayerController __instance)
+            {
+                PlayerController playercontroller = __instance as PlayerController;
+                float fwdMaxSpeed = QMod.Config.speedVal;
+                float seaglideAcl = QMod.Config.accelVal;
+                playercontroller.seaglideForwardMaxSpeed = fwdMaxSpeed;
+                playercontroller.seaglideWaterAcceleration = seaglideAcl;
+            }
+        }
+    }
+}
