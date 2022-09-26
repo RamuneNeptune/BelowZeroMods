24bacb4f9aac1d7a8c107a15a339339bac4c9c31
ramennoodlesxv <94365980+ramennoodlesxv@users.noreply.github.com>
2022-09-27 03:38:57 +1300

    Update SeaglideSpeed.cs

--- SeaglideSpeed_Source/SeaglideSpeed.cs
+++ SeaglideSpeed_Source/SeaglideSpeed.cs
@@ -10,10 +10,8 @@ namespace SeaglideSpeed_BZ
         [HarmonyPatch(nameof(SetMotorMode)), HarmonyPrefix]
         public static void SetMotorMode(Player.MotorMode newMotorMode, PlayerController __instance)
         {
-            if (newMotorMode != Player.MotorMode.Seaglide)
-            {
-                return;
-            }
+            if(newMotorMode != Player.MotorMode.Seaglide)
+            {return;}
             __instance.seaglideForwardMaxSpeed = Mathf.Lerp(20.0f, 30.00f, QMod.Config.accelVal);
             __instance.seaglideWaterAcceleration = Mathf.Lerp(28.56f, 56.00f, QMod.Config.accelVal);
         }
