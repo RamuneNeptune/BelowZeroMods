7ebe3585d9aa80ce971801ff7e6e0ff1427fda88
ramennoodlesxv <94365980+ramennoodlesxv@users.noreply.github.com>
2022-10-31 21:05:24 +1300

    Add files via upload

---
+++ V - 𝗠𝗮𝗶𝗻 𝗦𝗼𝘂𝗿𝗰𝗲/Item Backgrounds/Lists.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemBackgrounds
+{
+
+
+    internal class Tools
+    {
+        public static List<TechType> techTypes = new List<TechType>
+        {
+             TechType.Welder,
+             TechType.Scanner,
+             TechType.Knife,
+             TechType.Seaglide,
+             TechType.AirBladder,
+             TechType.PropulsionCannon,
+             TechType.Builder,
+             TechType.Flashlight,
+             TechType.LaserCutter,
+             TechType.MetalDetector,
+             TechType.TeleportationTool,
+             TechType.Thumper,
+             TechType.Flare,
+             TechType.LEDLight,
+             TechType.HeatBlade,
+             TechType.DiveReel,
+        };
+    }
+
+
+    internal class Resources
+    {
+        public static List<TechType> techTypes = new List<TechType>
+        {
+             TechType.Copper,
+             TechType.Sulphur,
+             TechType.Diamond,
+             TechType.Gold,
+             TechType.Kyanite,
+             TechType.Lead,
+             TechType.Lithium,
+             TechType.Magnetite,
+             TechType.ScrapMetal,
+             TechType.Nickel,
+             TechType.Quartz,
+             TechType.AluminumOxide,
+             TechType.Salt,
+             TechType.Silver,
+             TechType.Titanium,
+             TechType.UraniniteCrystal,
+        };
+    }
+
+    internal class Electronics
+    {
+        public static List<TechType> techTypes = new List<TechType>
+        {
+            TechType.AdvancedWiringKit,
+            TechType.Battery,
+            TechType.ComputerChip,
+            TechType.CopperWire,
+            TechType.PrecursorIonBattery,
+            TechType.PrecursorIonPowerCell,
+            TechType.RadioTowerPPU,
+            TechType.PowerCell,
+            TechType.ReactorRod,
+            TechType.RadioTowerTOM,
+            TechType.WiringKit,
+        };
+    }
+
+    internal class Synthetics
+    {
+        public static List<TechType> techTypes = new List<TechType>
+        {
+             TechType.EnameledGlass,
+             TechType.Glass,
+             TechType.FiberMesh,
+             TechType.Lubricant,
+             TechType.PlasteelIngot,
+             TechType.Silicone,
+             TechType.TitaniumIngot,
+             TechType.Aerogel,
+             TechType.FrozenCreatureAntidote,
+             TechType.Benzene,
+             TechType.HydraulicFluid,
+             TechType.HydrochloricAcid,
+             TechType.Benzene,
+             TechType.Polyaniline,
+             TechType.AramidFibers,
+        };
+    }
+
+
+    internal class LandFlora
+    {
+        public static List<TechType> techTypes = new List<TechType>
+        {
+             TechType.PurpleVegetable,
+             TechType.HeatFruit,
+             TechType.FrozenRiverPlant2,
+             TechType.LeafyFruit,
+             TechType.HangingFruit,
+             TechType.SmallMelon,
+             TechType.Melon,
+             TechType.SnowStalkerFruit,
+             TechType.SnowStalkerPlantLeaf,
+        };
+    }
+
+
+    internal class WaterFlora
+    {
+        public static List<TechType> techTypes = new List<TechType>
+        {
+             TechType.SmallMaroonPlantSeed,
+             TechType.TwistyBridgesMushroomChunk,
+             TechType.CreepvineSeedCluster,
+             TechType.JellyPlant,
+             TechType.JellyPlantSeed,
+             TechType.RedBushSeed,
+             TechType.GenericRibbonSeed,
+             TechType.GenericRibbon,
+             TechType.GenericSpiral,
+             TechType.GenericSpiralChunk,
+             TechType.PurpleStalkSeed,
+             TechType.DeepLilyShroom,
+             TechType.KelpRootPustule,
+             TechType.CreepvinePiece,
+        };
+    }
+
+
+    internal class RawFish
+    {
+        public static List<TechType> techTypes = new List<TechType>
+        {
+             TechType.ArcticPeeper,
+             TechType.ArrowRay,
+             TechType.Bladderfish,
+             TechType.Boomerang,
+             TechType.DiscusFish,
+             TechType.FeatherFish,
+             TechType.FeatherFishRed,
+             TechType.Hoopfish,
+             TechType.NootFish,
+             TechType.Spinefish,
+             TechType.SpinnerFish,
+             TechType.Symbiote,
+             TechType.Triops,
+        };
+    }
+
+
+    internal class CookedFish
+    {
+        public static List<TechType> techTypes = new List<TechType>
+        {
+             TechType.CookedArcticPeeper,
+             TechType.CookedArrowRay,
+             TechType.CookedBladderfish,
+             TechType.CookedBoomerang,
+             TechType.CookedDiscusFish,
+             TechType.CookedFeatherFish,
+             TechType.CookedFeatherFishRed,
+             TechType.CookedHoopfish,
+             TechType.CookedNootFish,
+             TechType.CookedSpinefish,
+             TechType.CookedSpinnerfish,
+             TechType.CookedSymbiote,
+             TechType.CookedTriops,
+        };
+    }
+
+
+    internal class Equipable
+    {
+        public static List<TechType> techTypes = new List<TechType>
+        {
+             TechType.SuitBoosterTank,
+             TechType.FlashlightHelmet,
+             TechType.ColdSuit,
+             TechType.ColdSuitGloves,
+             TechType.ColdSuitHelmet,
+             TechType.Fins,
+             TechType.SwimChargeFins,
+             TechType.UltraGlideFins,
+             TechType.Tank,
+             TechType.DoubleTank,
+             TechType.HighCapacityTank,
+             TechType.PlasteelTank,
+             TechType.SuitBoosterTank,
+             TechType.Rebreather,
+             TechType.ReinforcedDiveSuit,
+             TechType.ReinforcedGloves,
+             TechType.WaterFiltrationSuit,
+        };
+    }
+}
---
+++ Λ - 𝗢𝘁𝗵𝗲𝗿 𝗦𝗼𝘂𝗿𝗰𝗲/Seaglide Speed/SeaglideSpeed.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using Logger = QModManager.Utility.Logger;
+
+namespace SeaglideSpeed_BZ
+{
+    [HarmonyPatch(typeof(PlayerController))]
+    public static class PlayerControllerPatches
+    {
+        [HarmonyPatch(nameof(SetMotorMode)), HarmonyPrefix]
+        public static void SetMotorMode(Player.MotorMode newMotorMode, PlayerController __instance)
+        {
+            if(newMotorMode != Player.MotorMode.Seaglide)
+            {return;}
+            __instance.seaglideForwardMaxSpeed = Mathf.Lerp(20.0f, 30.00f, QMod.Config.accelVal);
+            __instance.seaglideWaterAcceleration = Mathf.Lerp(28.56f, 56.00f, QMod.Config.accelVal);
+        }
+    }
+}
