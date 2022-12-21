00caf2c861240b47528fc53d0d61a46541c89f2e
Ramune Neptune <94365980+RamuneNeptune@users.noreply.github.com>
2022-12-21 15:18:31 +1300

    Add files via upload

---
+++ Λ - 𝗢𝘁𝗵𝗲𝗿 𝗦𝗼𝘂𝗿𝗰𝗲/Debug Hotkeys/Patch.cs
@@ -0,0 +1,72 @@
+﻿
+
+using Logger = QModManager.Utility.Logger;
+using UnityEngine;
+using HarmonyLib;
+using System.Threading.Tasks;
+
+namespace DebugHotkeysBZ
+{
+    [HarmonyPatch(typeof(Player))]
+    public static class PlayerPatches
+    {
+        [HarmonyPatch(nameof(Player.Update))]
+        [HarmonyPostfix]
+        public static void Player_Update_Postfix(Player __instance)
+        {
+            if (Input.GetKeyDown(QMM.Config.LogHotkeys))
+            {
+                Logger.Log(Logger.Level.Info,
+                    $"<color=#007EC2>{QMM.Config.LogHotkeys} -</color> display hotkeys\n" +
+                    $"<color=#007EC2>{QMM.Config.CommandGhost} -</color> ghost \n" +
+                    $"<color=#007EC2>{QMM.Config.CommandWarpForward} -</color> warpforward 1 \n" +
+                    $"<color=#007EC2>{QMM.Config.CommandSpeed} -</color> speed 1\n" +
+                    $"<color=#007EC2>{QMM.Config.CommandSpeedHalf} -</color> speed 0.5 \n" +
+                    $"<color=#007EC2>{QMM.Config.CommandFog} -</color> fog \n" +
+                    $"<color=#007EC2>{QMM.Config.CommandSetDay} -</color> day \n" +
+                    $"<color=#007EC2>{QMM.Config.CommandSetNight} -</color> night",
+                    null, true);
+            }
+
+            if (Input.GetKeyDown(QMM.Config.CommandGhost))
+            {
+                DevConsole.SendConsoleCommand("ghost");
+                Logger.Log(Logger.Level.Info, "<color=#AFEDFA>Toggled 'ghost' command</color>", null, true);
+            }
+
+            if (Input.GetKeyDown(QMM.Config.CommandWarpForward))
+            {
+                DevConsole.SendConsoleCommand("warpforward 1");
+                Logger.Log(Logger.Level.Info, "<color=#AFEDFA>Sent 'warpforward 1' command</color>", null, true);
+            }
+
+            if (Input.GetKeyDown(QMM.Config.CommandSpeed))
+            {
+                DevConsole.SendConsoleCommand("speed 1");
+            }
+
+            if (Input.GetKeyDown(QMM.Config.CommandSpeedHalf))
+            {
+                DevConsole.SendConsoleCommand("speed 0.5");
+            }
+            
+            if (Input.GetKeyDown(QMM.Config.CommandFog))
+            {
+                DevConsole.SendConsoleCommand("fog");
+                Logger.Log(Logger.Level.Info, "<color=#AFEDFA>Sent 'fog' command</color>", null, true);
+            }
+            
+            if (Input.GetKeyDown(QMM.Config.CommandSetDay))
+            {
+                DevConsole.SendConsoleCommand("day");
+                Logger.Log(Logger.Level.Info, "<color=#AFEDFA>Set time to day</color>", null, true);
+            }
+
+            if (Input.GetKeyDown(QMM.Config.CommandSetNight))
+            {
+                DevConsole.SendConsoleCommand("night");
+                Logger.Log(Logger.Level.Info, "<color=#AFEDFA>Set time to night</color>", null, true);
+            }
+        }
+    }
+}
---
+++ Λ - 𝗢𝘁𝗵𝗲𝗿 𝗦𝗼𝘂𝗿𝗰𝗲/Debug Hotkeys/QMM.cs
@@ -0,0 +1,54 @@
+﻿
+
+using QModManager.API.ModLoading;
+using System.Reflection;
+using HarmonyLib;
+using SMLHelper.V2.Handlers;
+using SMLHelper.V2.Options.Attributes;
+using SMLHelper.V2.Json;
+using UnityEngine;
+
+namespace DebugHotkeysBZ
+{
+    [QModCore]
+    public static class QMM
+    {
+        public static MyConfig Config { get; } = OptionsPanelHandler.RegisterModOptions<MyConfig>();
+
+        [QModPatch]
+        public static void Patch()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var modName = ($"KentuckyFriedWaffle_{assembly.GetName().Name}");
+            Harmony harmony = new Harmony(modName);
+            harmony.PatchAll(assembly);
+        }
+    }
+    [Menu("Command Hotkeys BZ")]
+    public class MyConfig : ConfigFile
+    {
+        [Keybind("Display Hotkeys", Tooltip = "Keybind used to display hotkeys while in game")]
+        public KeyCode LogHotkeys = KeyCode.Comma;
+
+        [Keybind("<color=#AFEDFA>ghost</color>")]
+        public KeyCode CommandGhost = KeyCode.F2;
+
+        [Keybind("<color=#AFEDFA>warpforward 1</color>")]
+        public KeyCode CommandWarpForward = KeyCode.F4;
+        
+        [Keybind("<color=#AFEDFA>timescale 1</color>")]
+        public KeyCode CommandSpeed = KeyCode.F5;
+        
+        [Keybind("<color=#AFEDFA>timescale 0.5</color>")]
+        public KeyCode CommandSpeedHalf = KeyCode.F6;
+
+        [Keybind("<color=#AFEDFA>fog</color>")]
+        public KeyCode CommandFog = KeyCode.F7;
+
+        [Keybind("<color=#AFEDFA>day</color>")]
+        public KeyCode CommandSetDay = KeyCode.F9;
+
+        [Keybind("<color=#AFEDFA>night</color>")]
+        public KeyCode CommandSetNight = KeyCode.F10;
+    }
+}
